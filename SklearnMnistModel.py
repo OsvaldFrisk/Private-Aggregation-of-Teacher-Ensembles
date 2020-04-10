014bde02c69ddf7ad86517d384c00602ed22c4dc
Osvald Frisk <osvald.frisk@gmail.com>
2020-04-10 21:54:11 +0200

    Implemented example using the random forest classifier from scikit learn

---
+++ SklearnMnistModel.py
@@ -0,0 +1,43 @@
+from sklearn.model_selection import train_test_split
+from sklearn import datasets, metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import fetch_openml
+from pathlib import Path
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import time
+
+from BaseModel import BaseModel
+
+
+def load_mnist(validation_size=0.9, shuffle=False):
+    name = 'mnist_784'
+    dir_path = './cache/'
+    cache_path_X = dir_path + name + '_X.npy'
+    cache_path_y = dir_path + name + '_y.npy'
+    try:
+        X = np.load(cache_path_X)
+        y = np.load(cache_path_y)
+    except:
+        X, y = fetch_openml(name=name, version=1, cache=True, return_X_y=True)
+        y = y.astype(int)
+
+        Path(dir_path).mkdir(parents=True, exist_ok=True)
+        np.save(cache_path_X, X)
+        np.save(cache_path_y, y)
+
+    X_train, X_validation, y_train, y_validation = train_test_split(
+        X, y, test_size=validation_size, shuffle=shuffle)
+    return X_train, X_validation, y_train, y_validation
+
+
+class RFC(BaseModel):
+    def __init__(self):
+        self._model = RandomForestClassifier(max_depth=10)
+
+    def fit(self, X, y):
+        return self._model.fit(X, y)
+
+    def predict(self, X):
+        return self._model.predict(X)
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from Teacher import Teacher
-from SimpleModel import SimpleModel
+from SklearnMnistModel import load_mnist, RFC
+from sklearn.metrics import accuracy_score
 import numpy as np
 
+# from SimpleModel import SimpleModel
+# X_train = np.random.random((20, 12))
+# y_train = np.random.random((20))
+# X_validation = np.random.random((10, 12))
 
-X_train = np.random.random((20, 12))
-y_train = np.random.random((20))
+X_train, X_validation, y_train, y_validation = load_mnist()
 
-X_validation = np.random.random((10, 12))
-
-teachers = Teacher(SimpleModel, epochs=100, verbose=True, n_classes=2)
+teachers = Teacher(RFC, epochs=1, verbose=True, n_classes=10)
 teachers.fit(X_train, y_train)
-teachers.predict(X_validation)
+predicted = teachers.predict(X_validation)
+
+print(f"Accuracy: {accuracy_score(y_validation, predicted)}")
