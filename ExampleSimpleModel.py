43180548e71ceb9a7204f0039489a35611e14ab8
Osvald Frisk <osvald.frisk@gmail.com>
2020-04-11 15:39:19 +0200

    Old example using SimpleModel revised

---
+++ ExampleSimpleModel.py
@@ -0,0 +1,13 @@
+from PATE.Teacher import Teacher
+from PATE.SimpleModel import SimpleModel
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+X_train = np.random.random((20, 12))
+y_train = np.random.random((20))
+X_validation = np.random.random((10, 12))
+
+teachers = Teacher(SimpleModel, epochs=10, n_teachers=50,
+                   verbose=True, n_classes=10)
+teachers.fit(X_train, y_train)
+predicted = teachers.predict(X_validation)
